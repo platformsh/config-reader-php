c904b72d323f05f4aa1aadc34858746b65223e81
Larry Garfield <larry@platform.sh>
2019-02-05 14:33:39 -0600

    Add support code for more complete test cases.

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -6,6 +6,49 @@ use PHPUnit\Framework\TestCase;
 
 class ConfigTest extends TestCase
 {
+
+    /**
+     * A mock environment to simulate build time.
+     *
+     * @var array
+     */
+    protected $mockEnvironmentBuild = [];
+
+    /**
+     * A mock environment to simulate runtime.
+     *
+     * @var array
+     */
+    protected $mockEnvironmentDeploy = [];
+
+    public function setUp()
+    {
+        $env = $this->loadJsonFile('ENV');
+
+        // These sub-values are always encoded.
+        foreach (['PLATFORM_APPLICATION', 'PLATFORM_VARIABLES'] as $item) {
+            $env[$item] = $this->encode($this->loadJsonFile($item));
+        }
+
+        $this->mockEnvironmentBuild = $env;
+
+        // These sub-values are always encoded.
+        foreach (['PLATFORM_ROUTES', 'PLATFORM_RELATIONSHIPS'] as $item) {
+            $env[$item] = $this->encode($this->loadJsonFile($item));
+        }
+
+        $envRuntime = $this->loadJsonFile('ENV_runtime');
+        $env = array_merge($env, $envRuntime);
+
+        $this->mockEnvironmentDeploy = $env;
+
+    }
+
+    protected function loadJsonFile(string $name) : array
+    {
+        return json_decode(file_get_contents("tests/valid/{$name}.json"), true);
+    }
+
     public function testConfig()
     {
         //$this->expectException(\Exception::class);
